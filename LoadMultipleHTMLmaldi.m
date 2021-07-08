3a8579187835d94e583ca3adbfa11592e48b1bac
AbrahamAvelar <jabrahamavelar@gmail.com>
2021-07-07 23:01:20 -0500

    Update LoadMultipleHTMLmaldi.m

--- LoadMultipleHTMLmaldi.m
+++ LoadMultipleHTMLmaldi.m
@@ -1,13 +1,18 @@
 function AllData = LoadMultipleHTMLmaldi(directorio)
+% AllData = LoadMultipleHTMLmaldi(directorio)
+% Acumula en AllData los 4 outputs de loadHtmlMaldi para todos los .csv que
+% esten en el directorio input
 cwd=pwd;
 cd(directorio)
 Archivos=dir('*.csv');
 for i = 1:length(Archivos)
     i/length(Archivos)
     Archivo=Archivos(i).name;
-    [HtmlData, MatrixScores, AllStrainNames, MatrixScoresRank] = loadTophit(Archivo);
+    [HtmlData, MatrixScores, AllStrainNames, MatrixScoresRank] = loadHtmlMaldi(Archivo);
     AllData(i).filename=Archivo;
     AllData(i).Names=HtmlData;
     AllData(i).Scores=MatrixScores;
+    AllData(i).MatrixScoresRank=MatrixScoresRank;
+	AllData(i).AllStrainNames=AllStrainNames;
 end
 cd(cwd)
